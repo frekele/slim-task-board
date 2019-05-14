fa1bb037f60ac5329e8600ca2eb2f0c6930f124f
Leandro Kersting de Freitas <leandro@frekele.org>
2019-05-14 19:51:02 -0300

    TaskController

---
+++ back-end/app/controllers/TaskController.php
@@ -0,0 +1,61 @@
+<?php
+
+include_once '../model/entity/Task.php';
+include_once '../model/dao/TaskDAO.php';
+
+
+class TaskController
+{
+
+    public function insert($request, $response, $args)
+    {
+        $p = $request->getParsedBody();
+        $produto = new Produto(0, $p['nome'], $p['preco']);
+
+        $dao = new ProdutoDAO;
+        $produto = $dao->inserir($produto);
+
+        return $response->withJson($produto, 201);
+    }
+
+    public function update($request, $response, $args)
+    {
+        $id = $args['id'];
+        $p = $request->getParsedBody();
+        $produto = new Produto($id, $p['nome'], $p['preco']);
+
+        $dao = new ProdutoDAO;
+        $produto = $dao->atualizar($produto);
+
+        return $response->withJson($produto);
+    }
+
+    public function delete($request, $response, $args)
+    {
+        $id = $args['id'];
+
+        $dao = new ProdutoDAO;
+        $produto = $dao->deletar($id);
+
+        return $response->withJson($produto);
+    }
+
+    public function findAll($request, $response, $args)
+    {
+        $dao = new ProdutoDAO;
+        $produtos = $dao->listar();
+
+        return $response->withJson($produtos);
+    }
+
+    public function findById($request, $response, $args)
+    {
+        $id = $args['id'];
+
+        $dao = new ProdutoDAO;
+        $produto = $dao->buscarPorId($id);
+
+        return $response->withJson($produto);
+    }
+
+}
