b095dfcf507b72cfbdd5986bf326210c5d09fe32
Leandro Kersting de Freitas <leandro@frekele.org>
2019-05-14 20:54:44 -0300

    index

--- back-end/app/model/core/PDOFactory.php
+++ back-end/app/model/core/PDOFactory.php
@@ -8,9 +8,9 @@ class PDOFactory
     {
         if (!isset($pdo)) {
             $host = "192.168.9.11";
-            $dbName = "demo-slim";
-            $dbUser = "demo-slim-user";
-            $dbPass = "demo123456";
+            $dbName = "slim_task_board";
+            $dbUser = "slim-user";
+            $dbPass = "slim12345";
             $connection = "pgsql:dbname=$dbName;host=$host";
             $pdo = new PDO($connection, $dbUser, $dbPass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
