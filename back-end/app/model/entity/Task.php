f8377e232445d76b5d28ae363c9f6d01e8516a34
Leandro Kersting de Freitas <leandro@frekele.org>
2019-05-14 20:28:24 -0300

    Task

--- back-end/app/model/entity/Task.php
+++ back-end/app/model/entity/Task.php
@@ -6,14 +6,16 @@ class Task
     public $id;
     public $columnId;
     public $name;
+    public $weight;
     public $description;
     public $assignedUserId;
 
-    function __construct($id, $columnId, $name, $description, $assignedUserId)
+    function __construct($id, $columnId, $name, $weight, $description, $assignedUserId)
     {
         $this->id = $id;
         $this->columnId = $columnId;
         $this->name = $name;
+        $this->weight = $weight;
         $this->description = $description;
         $this->assignedUserId = $assignedUserId;
 
