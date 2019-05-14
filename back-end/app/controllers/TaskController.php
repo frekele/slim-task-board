f353d55f96b1363e7fa53e261dc5b199c8c9307c
Leandro Kersting de Freitas <leandro@frekele.org>
2019-05-14 20:21:35 -0300

    TaskController

--- back-end/app/controllers/TaskController.php
+++ back-end/app/controllers/TaskController.php
@@ -9,53 +9,64 @@ class TaskController
 
     public function insert($request, $response, $args)
     {
-        $p = $request->getParsedBody();
-        $produto = new Produto(0, $p['nome'], $p['preco']);
-
-        $dao = new ProdutoDAO;
-        $produto = $dao->inserir($produto);
-
-        return $response->withJson($produto, 201);
+        try {
+            $p = $request->getParsedBody();
+            $toard = new Task(0, $p['name'], $p['description']);
+            $dao = new TaskDAO;
+            $toard = $dao->insert($toard);
+        } catch (Exception $error) {
+            return $response->withStatus(500);
+        }
+        return $response->withJson($toard, 201);
     }
 
     public function update($request, $response, $args)
     {
-        $id = $args['id'];
-        $p = $request->getParsedBody();
-        $produto = new Produto($id, $p['nome'], $p['preco']);
-
-        $dao = new ProdutoDAO;
-        $produto = $dao->atualizar($produto);
-
-        return $response->withJson($produto);
+        try {
+            $id = $args['id'];
+            $p = $request->getParsedBody();
+            $toard = new Task($id, $p['name'], $p['description']);
+            $dao = new TaskDAO;
+            $toard = $dao->update($toard);
+        } catch (Exception $error) {
+            return $response->withStatus(500);
+        }
+        return $response->withJson($toard);
     }
 
     public function delete($request, $response, $args)
     {
-        $id = $args['id'];
-
-        $dao = new ProdutoDAO;
-        $produto = $dao->deletar($id);
-
-        return $response->withJson($produto);
+        try {
+            $id = $args['id'];
+            $dao = new TaskDAO;
+            $toard = $dao->delete($id);
+        } catch (Exception $error) {
+            return $response->withStatus(500);
+        }
+        return $response->withJson($toard);
     }
 
     public function findAll($request, $response, $args)
     {
-        $dao = new ProdutoDAO;
-        $produtos = $dao->listar();
-
-        return $response->withJson($produtos);
+        try {
+            $dao = new TaskDAO;
+            $toards = $dao->findAll();
+        } catch (Exception $error) {
+            return $response->withStatus(500);
+        }
+        return $response->withJson($toards);
     }
 
     public function findById($request, $response, $args)
     {
-        $id = $args['id'];
-
-        $dao = new ProdutoDAO;
-        $produto = $dao->buscarPorId($id);
-
-        return $response->withJson($produto);
+        try {
+            $id = $args['id'];
+            $dao = new TaskDAO;
+            $toard = $dao->findById($id);
+        } catch (Exception $error) {
+            return $response->withStatus(500);
+        }
+        return $response->withJson($toard);
     }
 
 }
